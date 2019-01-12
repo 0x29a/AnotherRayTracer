bd420715a3df6d9a75777d20f8b65b06b6701561
true-datura <grrrr@protonmail.com>
2019-01-12 18:34:36 +0200

    Save red sphere projection to ppm file

--- ray_tracer_yay.c
+++ ray_tracer_yay.c
@@ -49,6 +49,14 @@ bool isRayIntersectsSphere(Ray *ray, Sphere *sphere) {
         return true;
 }
 
+
+void saveppm(char * filename, unsigned char * image, unsigned int width, unsigned int height) {
+    FILE * imageFile = fopen(filename, "wb");
+    fprintf(imageFile, "P6 %d %d %d\n", width, height, 255);
+    fwrite(image, 3, width * height, imageFile);
+    fclose(imageFile);
+}
+
 int main() {
     Ray ray = {
         {0, 0, 0},
@@ -56,23 +64,29 @@ int main() {
     };
 
     Sphere sphere = {
-        10,
-        {20, 20, 20}
+        100,
+        {200, 200, 200}
     };
 
-    for (int y=0; y<40; y++) {
-        ray.start.y = y;
-        for (int x=0; x<40; x++) {
-            ray.start.x = x;
+    char image[160000 * 3];
 
-            if (isRayIntersectsSphere(&ray, &sphere)) {
-                printf("++");
-            }
-            else {
-                printf("--");
-            }
+
+    unsigned int counter = 0;
+
+
+    for (int y=0; y<400; y++) {
+        ray.start.y = y;
+        for (int x=0; x<400; x++) {
+            ray.start.x = x;
+            image[counter] = (isRayIntersectsSphere(&ray, &sphere)) ? 255: 0;
+            image[counter + 1] = 0;
+            image[counter + 2] = 0;
+            counter += 3;
         }
         printf("\n");
     }
+
+    saveppm("test.ppm", image, 400, 400);
+
     return 1;
 }
